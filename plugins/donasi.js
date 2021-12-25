aa03896f0f233c5119c48b2f6a0499dc9c9b0b3c
bayubotzwa <96553504+bayubotzwa@users.noreply.github.com>
2021-12-25 10:22:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,12 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+│ • Axis [089525518325]
+│ • Telkomsel [081285866502]
 ╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
+│ • Gopay,Dana [089525518325]
+│ • Saweria : -
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
